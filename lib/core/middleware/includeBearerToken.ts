f887e1b2c6149dbffd86822845982371f63a3a40
vemundeldegard <vemundeldegard@gmail.com>
2022-06-03 21:15:43 +0200

    AV-582: Add bearer token middleware for OAuth2 apps

---
+++ lib/core/middleware/includeBearerToken.ts
@@ -0,0 +1,15 @@
+import { RequestOperation } from "../../application/operation";
+import { Bundle } from "../bundle";
+import { F } from "../f";
+
+export const includeBearerToken = (
+  request: RequestOperation,
+  f: F,
+  bundle: Bundle
+) => {
+  const authData = bundle.authData as Record<string, string>;
+  if (authData.access_token && request.headers) {
+    request.headers["Authorization"] = `Bearer ${authData.access_token}`;
+  }
+  return request;
+};
