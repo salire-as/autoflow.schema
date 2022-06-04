78dcdd3105b9421c0e13b181d2ab74369fba2b5d
vemundeldegard <36489034+vemundeldegard@users.noreply.github.com>
2022-06-04 11:49:32 +0200

    AV-582 (#14)
    * AV-582: Apps should test authentication and can set a label
    
    * Bump version

--- lib/application/authentication/index.ts
+++ lib/application/authentication/index.ts
@@ -14,5 +14,14 @@ interface OAuth2Operation {
 
 export interface Authentication {
   type: AUTHENTICATION_TYPE;
+  /**
+   * A test is required to check if authentication is working as it should. What is returned from the `test` method is available
+   * in `label`.
+   */
+  test: RequestOperation | RequestFunction;
+  /**
+   * Label defines what users see in the list of their authenticated accounts.
+   */
+  label?: RequestFunction;
   oauth2?: OAuth2Operation;
 }
