1ccd90cefebc80b4543914887056737badfce599
vemundeldegard <36489034+vemundeldegard@users.noreply.github.com>
2022-07-04 12:42:53 +0200

    AV-1284 (#21)
    * AV-1284
    
    * AV-1284

--- lib/application/common/index.ts
+++ lib/application/common/index.ts
@@ -3,11 +3,32 @@ import { Operation } from "../operation";
 export enum InputOutputType {
   STRING = "string",
   NUMBER = "number",
+  DYNAMIC = "dynamic",
+  DROPDOWN = "dropdown",
 }
 
 interface Choice {
   label: string;
   value: string;
+}
+
+export enum DROPDOWN_TYPE {
+  STATIC = "static",
+  DYNAMIC = "dynamic",
+}
+
+export enum VISIBILITY {
+  IMPORTANT = "important",
+  NORMAL = "normal",
+  HIDDEN = "hidden",
+}
+
+interface DropdownInputOutput {
+  type: DROPDOWN_TYPE;
+  /** For static dropdowns you need to define a list of values */
+  values?: Choice[];
+  /** For dynamic dropdowns you need to define the dropdown source, which should be a string */
+  source?: string;
 }
 
 interface InputOutput {
@@ -16,8 +37,13 @@ interface InputOutput {
   description: string;
   required?: boolean;
   type: InputOutputType;
-  choices?: Choice[];
   default?: string | number;
+  dropdown?: DropdownInputOutput;
+}
+
+export enum OPERATION_METHOD {
+  POLLING = "polling",
+  WEBHOOK = "webhook",
 }
 
 export interface ActionOrTrigger {
@@ -27,6 +53,8 @@ export interface ActionOrTrigger {
   inputs?: Inputs;
   outputs?: Outputs;
   operation: Operation;
+  visibility?: VISIBILITY;
+  type?: OPERATION_METHOD;
 }
 
 export type Inputs = InputOutput[];
