cfc7220f3cfef8a6315ad150871112d334d86e7e
vemundeldegard <36489034+vemundeldegard@users.noreply.github.com>
2022-07-06 12:04:42 +0200

    AV-1312 (#22)

--- lib/core/curlies/index.ts
+++ lib/core/curlies/index.ts
@@ -3,15 +3,22 @@ import { isString } from "lodash";
 import { RequestOperation } from "../../application";
 import { Bundle } from "../bundle";
 
-export const curlies = (
-  input: Record<string, unknown> | RequestOperation | string,
-  replacements: Record<string, unknown> | Bundle
-) => {
+export const curlies = <
+  T extends Record<string, unknown> | RequestOperation | string
+>(
+  input: T,
+  replacements: Record<string, unknown> | Bundle,
+  initialValueAsString: boolean = false
+): T => {
   const template = handlebars.compile(
     isString(input) ? input : JSON.stringify(input)
   );
 
   const output = template(replacements);
 
-  return isString(input) ? output : JSON.parse(output);
+  if (output.match(/({)?{{[^{}]*}}(?!})/g))
+    return curlies(output, replacements, initialValueAsString) as T;
+  else if (initialValueAsString) {
+    return output as T;
+  } else return JSON.parse(output) as T;
 };
