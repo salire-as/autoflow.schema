422a81ef9455d0a56721c09bb39d9083699e0768
vemundeldegard <vemundeldegard@gmail.com>
2022-07-06 12:02:16 +0200

    AV-1312

---
+++ lib/core/curlies/index.spec.ts
@@ -0,0 +1,29 @@
+import { curlies } from ".";
+
+describe("Curlies", () => {
+  it("Parses curlies and replaces them", () => {
+    const output = curlies(
+      {
+        headers: {},
+        params: {},
+        query: {},
+        data: {
+          content: "Hello {{bundle.input.62bdb803dc4284b9dadcf3fd.field}}",
+        },
+      },
+      {
+        bundle: {
+          input: {
+            deepNested: ", Oslo",
+            nested: "Eldegard{{bundle.input.deepNested}}",
+            "62bdb803dc4284b9dadcf3fd": {
+              field: "Vemund {{bundle.input.nested}}",
+            },
+          },
+        },
+      }
+    );
+
+    expect(output.data.content).toBe("Hello Vemund Eldegard, Oslo");
+  });
+});
